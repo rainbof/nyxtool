f5a92bd6158780886416f854a636b43b566a944a
Ondrej Beranek <rainbof@gmail.com>
2024-07-04 11:57:18 +0200

    functions are now loaded automatically

--- delete-conversation.sh
+++ delete-conversation.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 declare -a all_postid
-discussionId="284395" #zejtra to smazu
-#discussionId="284489" #jede bagr
+#discussionId="284395" #zejtra to smazu
+discussionId="284489" #jede bagr
 
-source functions/load_config.shl
-source functions/nyx_discussion_send.shl
+source functions/load_functions.shl
+load_functions
 
 config_file="config.conf"
-
 load_config
+
 response=$(curl -s -X GET "https://nyx.cz/api/discussion/${discussionId}" -H "Authorization: Bearer ${token}" -H 'accept: application/json')
 
 readarray -t all_postid < <(jq '.posts[] | select(.discussion_id == '${discussionId}') | .id' <<< "${response}")
@@ -25,4 +25,5 @@ for id in "${all_postid[@]}"; do
     printf 'deleted\n'
 done
 
-    nyx_discussion_send "Mam na vas otazku: mam diskusi i nadale zacinat nejakou uvodni vetou ? Nebo to mam nechat jentak ? Nabizi se i moznost ze by se tahle veta vylosovala. Ovsem je otazkou zda to nevybocuje ze zamysleneho ramce Zitra to smazu..." "${discussionId}" "text"
+message='Krleš'
+nyx_discussion_send "${message}" "${discussionId}" "text"
